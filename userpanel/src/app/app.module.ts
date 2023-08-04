3de97df53728b2ccb3172473d53f8e79f2d7ca3e
abhishekmaury <115974377+Achal123Eminence@users.noreply.github.com>
2023-08-04 16:40:58 +0530

    Changes Done

--- userpanel/src/app/app.module.ts
+++ userpanel/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
