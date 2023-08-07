ed9831dd338dd480296d1e2f5b6fb8ed5bcad887
abhishekmaury <115974377+Achal123Eminence@users.noreply.github.com>
2023-08-07 14:55:39 +0530

    Changes Done

--- userpanel/src/app/app-routing.module.ts
+++ userpanel/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { InplayComponent } from './inplay/inplay.component';
 import { MultimarketsComponent } from './multimarkets/multimarkets.component';
 import { HighlightsComponent } from './highlights/highlights.component';
+import { ChecksportwiseresultComponent } from './checksportwiseresult/checksportwiseresult.component';
 
 const routes: Routes = [
   { path:'', component:HomeComponent },
   { path:'inplay', component:InplayComponent },
   { path:'multimarkets', component:MultimarketsComponent },
-  { path:'highlights', component:HighlightsComponent }
+  { path:'highlights', component:HighlightsComponent },
+  { path:'checksportwiseresult', component:ChecksportwiseresultComponent }
 ];
 
 @NgModule({
